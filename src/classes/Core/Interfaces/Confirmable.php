4af69eede34e8dbec86d57a7c154eb8153429ccc
Andares Merigold <andares@outlook.com>
2017-08-11 12:16:53 +0800

    add interface confirmable

---
+++ src/classes/Core/Interfaces/Confirmable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Combi\Core\Interfaces;
+
+use Combi\{
+    Helper as helper,
+    Abort as abort,
+    Core as core
+};
+
+/**
+ *
+ * @author andares
+ */
+interface Confirmable {
+    public function confirm();
+}
