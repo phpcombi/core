3e1427d40c8b0eeef7460f082442b0efa703e8c7
Andares Merigold <andares@outlook.com>
2017-09-02 03:25:00 +0800

    add meta extension Accessors

---
+++ src/classes/Core/Meta/Extensions/Accessors.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Combi\Core\Meta\Extensions;
+
+/**
+ * 存取器支持
+ *
+ * @author andares
+ */
+trait Accessors {
+    public function __set($key, $value) {
+        $method = "_set_$key";
+        if (method_exists($this, $method)) {
+            $value = $this->$method($value);
+        }
+        $this->set($key, $value);
+    }
+
+    public function __get($key) {
+        $value  = $this->get($key);
+
+        $method = "_get_$key";
+        if (method_exists($this, $method)) {
+            $value = $this->$method($value);
+        }
+        return $value;
+    }
+
+    public function __isset($key) {
+        $method = "_isset_$key";
+        if (method_exists($this, $method)) {
+            return $this->$method($this->has($key));
+        }
+        return $this->has($key);
+    }
+
+    public function __unset($key) {
+        $method = "_unset_$key";
+        if (method_exists($this, $method)) {
+            if ($this->$method()) {
+                return;
+            }
+        }
+        $this->remove($key);
+    }
+}
